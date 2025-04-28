8026cef12daa84a075298029b50f0ed76a970442
navya23-git <navyasoma683@gmail.com>
2025-04-28 15:23:39 +0530

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,7 @@
+public class Library{
+public static void displaywelcomemessage(){
+System.out.println("welcome to the Library management System!");
+}
+public static void main(String[] args){
+displaywelcomemessage();
+}}
